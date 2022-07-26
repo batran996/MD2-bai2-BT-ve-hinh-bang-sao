5d1b790f4e22679529913e17acd854a12bca045e
unknown <bazo20061996@gmail.com>
2022-07-26 10:54:15 +0700

    MD2-bai2-BT ve hinh bang sao

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ve hinh nhe!");
+        System.out.println("nhập vào 1: vẽ hình chữ nhật");
+        System.out.println("nhập vào 2: vẽ hình tma giác đứng");
+        System.out.println("nhập vào 3: vẽ hình tam giác ngược");
+        System.out.println("nhập vào 4 để thoát ra");
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        int chieuDai;
+        int chieuRong;
+        int height;
+        if (input == 1){
+            System.out.println("nhập chiều dài");
+            chieuDai = scanner.nextInt();
+            System.out.println("nhập chiều rộng");
+            chieuRong = scanner.nextInt();
+            for (int i = 0; i < chieuRong; i++) {
+                for (int j = 0; j < chieuDai; j++) {
+                    System.out.print(" * ");
+                }
+                System.out.println(" ");
+            }
+        }
+        if (input == 2){
+            System.out.println("Nhập vào chiều cao tam giác");
+            height = scanner.nextInt();
+            for (int i = 1; i <= height; i++) {
+                for (int j =1 ; j <= i; j++) {
+                    System.out.print(" * ");
+                }
+                System.out.println(" ");
+            }
+        }
+        if (input == 3){
+            System.out.println("nhập vào chiều cao tam giác");
+            height = scanner.nextInt();
+            for (int i = height; i>=1 ; i--) {
+                for (int j = i; j >=1 ; j--) {
+                    System.out.print(" * ");
+                }
+                System.out.println(" ");
+            }
+        }
+
+    }
+}
